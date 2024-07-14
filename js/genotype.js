fd5b02638374b7a9afe80a9849003032c110d5e9
skub <skub@outlook.com>
2024-07-14 21:51:49 +0900

    changed

--- js/genotype.js
+++ js/genotype.js
@@ -1,4 +1,39 @@
-U = ID => document.getElementById(ID);
+U = {};
+U.$ = (ID) => document.getElementById(ID);
+//Text
+U.createTxt = (PID, STR) => U.$(PID).appendChild(document.createTextNode(STR));
+U.getTxt = (ID) => U.$(ID).firstChild.nodeValue;
+U.setTxt = (ID, STR) => U.$(ID).firstChild.nodeValue = STR;
+//Break
+U.createBrk = (PID) => U.$(PID).appendChild(document.createElemnt("br"));
+//Tag
+U.createTag = (PID, ID, TAG) => {
+  let obj = document.createElement(TAG);
+  obj.id = ID;
+  U.$(PID).appendChild(obj);
+};
+U.removeTag = (ID) => U.$(ID).parentNode.removeChild(U.$(ID));
+//Button
+U.makeSolidButton = (PID, ID, STR) => {
+  U.createTag(PID, ID, "button");
+  U.createTxt(ID, STR);
+};
+//TextBox
+U.makeTextLine = (PID, ID, SIZE) => {
+  let obj = document.createElement("input");
+  obj.id = ID;
+  obj.type = "text";
+  obj.size = SIZE;
+  U.$(PID).appendChild(obj);
+};
+U.makeTextArea = (PID, ID, COL, ROW) => {
+  let obj = document.createElement("textarea");
+  obj.id = ID;
+  obj.cols = COL;
+  obj.rows = ROW;
+  U.$(PID).appendChild(obj);
+};
+
 G = {};
 G.getCB = () => navigator.clipboard.readText();
 G.setCB = (PRO) => navigator.clipboard.writeText(PRO);
--- js/index.js
+++ js/index.js
@@ -1,11 +1,59 @@
 const KIT_TITLE = "Honeycomb";
-const HC_NUM = 5;
+const HC_NUM = 6;
+const BOX_SIZE = 80;
 let main = () => {
-    U("Title").appendChild(document.createTextNode(KIT_TITLE));
-    U("in").onclick = () => {
-        G.getCB().then(RES => U("box").value = RES);
-    };
-    U("out").onclick = () => {
-        G.setCB(U("box").value);
+    U.createTxt("Title", KIT_TITLE);
+    U.createTag("Container", "Table", "table");
+    for (let i = 1; i <= HC_NUM; i++) {
+        U.createTag("Table", "TabRow_"+i, "tr");
+        U.createTag("TabRow_"+i, "TabData_"+i+"_1", "td");
+        U.makeSolidButton("TabData_"+i+"_1", "unshift_"+i, "unshift");
+        U.createTag("TabRow_"+i, "TabData_"+i+"_2", "td");
+        U.makeSolidButton("TabData_"+i+"_2", "push_"+i, "push");
+        U.createTag("TabRow_"+i, "TabData_"+i+"_3", "td");
+        U.makeSolidButton("TabData_"+i+"_3", "swap_"+i, "swap");
+        U.createTag("TabRow_"+i, "TabData_"+i+"_4", "td");
+        U.makeSolidButton("TabData_"+i+"_4", "in_"+i, "IN");
+        U.createTag("TabRow_"+i, "TabData_"+i+"_5", "td");
+        U.makeTextLine("TabData_"+i+"_5", "box_"+i, BOX_SIZE);
+        U.createTag("TabRow_"+i, "TabData_"+i+"_6", "td");
+        U.makeSolidButton("TabData_"+i+"_6", "out_"+i, "OUT");
+        U.createTag("TabRow_"+i, "TabData_"+i+"_7", "td");
+        U.makeSolidButton("TabData_"+i+"_7", "copy_"+i, "copy");
+        U.$("unshift_"+i).onclick = ((I) => {
+            return () => {
+                G.getCB().then(RES => U.$("box_"+I).value = RES + U.$("box_"+I).value);
+            };
+        })(i);
+        U.$("push_"+i).onclick = ((I) => {
+            return () => {
+                G.getCB().then(RES => U.$("box_"+I).value = U.$("box_"+I).value + RES);
+            };
+        })(i);
+        U.$("swap_"+i).onclick = ((I) => {
+            return () => {
+                G.getCB().then(RES => {
+                    let str = U.$("box_"+I).value;
+                    U.$("box_"+I).value = RES;
+                    G.setCB(str);
+                });
+            };
+        })(i);
+        U.$("in_"+i).onclick = ((I) => {
+            return () => {
+                G.getCB().then(RES => U.$("box_"+I).value = RES);
+            };
+        })(i);
+        U.$("out_"+i).onclick = ((I) => {
+            return () => {
+                G.setCB(U.$("box_"+I).value);
+                U.$("box_"+I).value = "";
+            };
+        })(i);
+        U.$("copy_"+i).onclick = ((I) => {
+            return () => {
+                G.setCB(U.$("box_"+I).value);
+            };
+        })(i);
     };
 };
